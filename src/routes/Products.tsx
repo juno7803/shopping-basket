20e93a69d8c82a3c613ca61249f5901df5587d8c
junholee <juno7803@naver.com>
2020-08-27 23:25:45 +0900

    Product 페이지에 상품 리스트 출력(정렬 및 페이지네이션 이전)

--- src/routes/Products.tsx
+++ src/routes/Products.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import { productItems } from "../models/productItems";
+import { Row, Col, Card, Container, CardHeader, CardBody } from "reactstrap";
 
-function Products (){
-    return(<></>);
+function Products() {
+  return (
+    <Container>
+      {productItems.map((prod, index) => (
+        <Card>
+            <CardHeader>
+                {prod.title}
+            </CardHeader>
+            <CardBody>
+                <Row>
+                    <Col md="3">
+                        <img className="img-fluid" src={prod.coverImage} alt="Responsive image"/>
+                    </Col>
+                    <Col>
+                        가격 : {prod.price} 원
+                    </Col>
+                </Row>
+            </CardBody>
+        </Card>
+      ))}
+    </Container>
+  );
 }
 
 export default Products;
